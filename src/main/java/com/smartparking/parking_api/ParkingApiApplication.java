225e156960eafe3d4752549a5f568d9002fe0362
HajarAmenzou45 <hajaramenzou45@gmail.com>
2026-04-11 21:07:10 +0100

    fix port railway

--- src/main/java/com/smartparking/parking_api/ParkingApiApplication.java
+++ src/main/java/com/smartparking/parking_api/ParkingApiApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ParkingApiApplication {
 
+public class ParkingApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingApiApplication.class, args);
 	}
-
 }
